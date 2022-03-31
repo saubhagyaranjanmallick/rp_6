65c23a333a4fa2880c864988a79d2caaa62352cb
Saubhagya <96274810+saubhagyaranjanmallick@users.noreply.github.com>
2022-03-31 13:04:07 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Box, Card } from "@mui/material";
+import * as React from "react";
+import Searchbar from "./Searchbar";
+
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1 className="root">Movieland Industries...</h1>
+       
+      <Searchbar/>
+        <Box sx={{ flexGrow: 1 }}>
+          <Card >
+
+          </Card>
+        </Box>
+      </div>
     </div>
   );
 }
